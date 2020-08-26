0e72cb83f60d09b8c6896b491c0e93436b7888f1
Jack Hawinkels <jack.hawinkels@gmail.com>
2020-08-26 12:39:28 +1200

    Completed group page colour block

---
+++ 2.8 & 24 web project - EOTC website/groups/group_data.js
@@ -0,0 +1,15 @@
+let groups = [
+    {
+        id: 1,
+        data: ["7jhawinkels", "techangels", "techangels", "techangels", "techangels", "techangels", "techangels", "techangels", "techangels", "techangels", "techangels", "techangels", "techangels", "techangels", "techangels", "techangels", "ThePerson", "techangels", "techangels"],
+    },{
+        id: 2,
+        data: ["7jhawinkels", "techangels", "techangels", "techangels", "techangels", "techangels", "techangels", "techangels", "techangels", "techangels", "techangels", "techangels", "techangels", "techangels", "techangels", "techangels", "ThePerson", "techangels", "techangels"],
+    },{
+        id: 3,
+        data: ["7jhawinkels", "techangels", "techangels", "techangels", "techangels", "techangels", "techangels", "techangels", "techangels", "techangels", "techangels", "techangels", "techangels", "techangels", "techangels", "techangels", "ThePerson", "techangels", "techangels"],
+    },{
+        id: 4,
+        data: ["7jhawinkels", "techangels", "techangels", "techangels", "techangels", "techangels", "techangels", "techangels", "techangels", "techangels", "techangels", "techangels", "techangels", "techangels", "techangels", "techangels", "ThePerson", "techangels", "techangels"],
+    }
+]
